df55ccd309f57afb3d7ba77992513ff5aa0143d8
Oron Ben Zvi <oronbz@gmail.com>
2017-09-23 15:26:46 +0300

    Initial Commit

---
+++ Hitch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hitch
+//
+//  Created by Oron Ben Zvi on 9/23/17.
+//  Copyright © 2017 Oron Ben Zvi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
